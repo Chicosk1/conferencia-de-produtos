182047558bda445596240dd5e579ea3d2ecc3130
Chicoski <gabrielsamudiocichocki@gmail.com>
2024-11-11 21:20:55 -0300

    teste

--- src/main/java/dva/Main.java
+++ src/main/java/dva/Main.java
@@ -2,26 +2,22 @@ package dva;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class Main extends Application {
+
     @Override
-    public void start(Stage stage) throws IOException {
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("Conferencia.fxml"));
-            Scene scene = new Scene(root);
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Conferencia.fxml"));
+        Scene conferenciaScene = new Scene(loader.load());
 
-            stage.setTitle("Conferência de Estoque");
-            stage.setScene(scene);
-            stage.show();
+        ConferenciaController conferenciaController = loader.getController();
+        conferenciaController.inicializar(primaryStage);
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        primaryStage.setScene(conferenciaScene);
+        primaryStage.setTitle("Tela de Conferência");
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
